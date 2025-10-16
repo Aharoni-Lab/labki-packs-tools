eb2170729d3b296cfc81516b909b146b195fc3e0
Daniel Aharoni <dbaharoni@gmail.com>
2025-10-16 14:22:27 -0700

    remove pack semver validator since this is already enforced by schema

--- src/labki_packs_tools/validation/validators/__init__.py
+++ src/labki_packs_tools/validation/validators/__init__.py
@@ -3,7 +3,6 @@ from .orphan_page_validator import OrphanPageValidator
 from .pack_cycle_validator import PackCycleValidator
 from .pack_dependency_validator import PackDependencyValidator
 from .pack_pages_validator import PackPagesValidator
-from .pack_semver_validator import PackSemverValidator
 from .page_file_validator import PageFileValidator
 
 __all__ = [
@@ -12,6 +11,5 @@ __all__ = [
     "PackCycleValidator",
     "PackDependencyValidator",
     "PackPagesValidator",
-    "PackSemverValidator",
     "PageFileValidator",
 ]
